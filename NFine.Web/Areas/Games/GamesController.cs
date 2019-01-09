098064c4e81d64a64093ecf523f0a867e3576e9e
Administrator <Administrator@PC-20181021JRDG>
2019-01-09 17:01:37 +0800

    update

---
+++ NFine.Web/Areas/Games/GamesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Areas.Games
+{
+    public class GamesController : Controller
+    {
+        //
+        // GET: /Games/Games/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shenjingmao2()
+        {
+            return View();
+        }
+
+    }
+}
